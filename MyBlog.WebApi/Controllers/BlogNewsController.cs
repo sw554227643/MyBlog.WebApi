95ad27c50d210bd135f699156cff25bf092e8818
visioner <554227643@qq.com>
2022-03-30 22:42:45 +0800

    增加增删改查

--- MyBlog.WebApi/Controllers/BlogNewsController.cs
+++ MyBlog.WebApi/Controllers/BlogNewsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyBlog.IService;
+using MyBlog.Model;
+using MyBlog.WebApi.Utility.ApiResult;
+using System;
 using System.Threading.Tasks;
 
 namespace MyBlog.WebApi.Controllers
@@ -16,11 +19,70 @@ namespace MyBlog.WebApi.Controllers
             this._iBlogNewsService = iBlogNewsService;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GetBlogNews()
+        [HttpGet("GetBlogNews")]
+        public async Task<ActionResult<ApiResult>> GetBlogNews()
         {
-            var data = await  _iBlogNewsService.QueryAsync();
-            return Ok(data);
+            var data = await _iBlogNewsService.QueryAsync();
+            if (data is null)
+            {
+                return ApiResultHelper.Error("data is null");
+            }
+            return ApiResultHelper.Success(data);
+        }
+
+        /// <summary>
+        /// 添加文章
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        [HttpPost("Create")]
+        public async Task<ActionResult<ApiResult>> Create(string title, string content, int typeid)
+        {
+            //数据验证
+            BlogNews blogNews = new BlogNews
+            {
+                BrowseCount = 0,
+                Title = title,
+                Content = content,
+                Time = DateTime.Now,
+                WriterId = 1,
+                TypeId = typeid,
+                LikeCount = 0
+            };
+            bool b = await _iBlogNewsService.CreateAsync(blogNews);
+            if (!b)
+            {
+                return ApiResultHelper.Error("添加失败：服务器发生错误");
+            }
+            return ApiResultHelper.Success(blogNews);
+        }
+
+        [HttpPost("Update")]
+        public async Task<ActionResult<ApiResult>> Update(int id,string title,string content,int typeid)
+        {
+            var blognews = await _iBlogNewsService.GetByIdAsync(id);
+            if (blognews is null)
+            {
+                return ApiResultHelper.Error("未找到改文章");
+            }
+            else
+            {
+                blognews.Title = title;
+                blognews.Content = content; 
+                blognews.TypeId = typeid;
+                var  b = await _iBlogNewsService.EditAsync(blognews);
+                if (!b) return ApiResultHelper.Error("修改失败");
+                return ApiResultHelper.Success(blognews);
+            }
+        }
+
+        [HttpPost("Delete")]
+        public async Task<ActionResult<ApiResult>> Delete(int id)
+        {
+            var b = await _iBlogNewsService.DeleteAsync(id);
+            if (!b) return ApiResultHelper.Error("删除失败");
+            return ApiResultHelper.Success(b);
         }
 
     }
---
+++ MyBlog.WebApi/Utility/ApiResult/ApiResult.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlog.WebApi.Utility.ApiResult
+{
+    public class ApiResult
+    {
+
+        public int Code { get; set; }
+        public string Msg { get; set; }
+        public int Total { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
